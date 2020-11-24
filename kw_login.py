685990e8df70a0b8150e1abcbc206996a7dc111f
unknown <kumarnisit@gmail.com>
2020-11-24 18:54:37 +0530

    fixing logout and moving report task functionalities

--- kw_login.py
+++ kw_login.py
@@ -6,9 +6,6 @@ from getopt import getopt
 import os, time, random, argparse
 
 URL='https://portal.csm.co.in'
-login_time=datetime(year=2020, month=9, day=30, hour=8, minute=40, second=0)
-logoff_time=datetime(year=2020, month=9, day=30, hour=19, minute=30, second=0)
-cur_time=datetime.now()
 
 my_parser = argparse.ArgumentParser(allow_abbrev=False)
 my_parser.add_argument('--username', action='store', type=str, required=True, metavar='', help='Username for logging in portal')
@@ -17,7 +14,9 @@ my_parser.add_argument('--browser', action='store', type=str, required=True, met
 my_parser.add_argument('--check_in', action='store_true',
 						help='Willing to check in, include this flag if want to set as true, default:False *Optional')
 my_parser.add_argument('--log_out', action='store_true', 
-						help='Willing to log out, include this flag if want to set as true, default:False *Optional')k
+						help='Willing to log out, include this flag if want to set as true, default:False *Optional')
+my_parser.add_argument('--report_task', action='store_true',
+						help='Willing to report task, include this flag if want to set as true, default:False *Optional')
 args = my_parser.parse_args()
 
 class kw_login:
@@ -43,7 +42,8 @@ class kw_login:
 		password.send_keys(Keys.RETURN)
 		time.sleep(random.randrange(6,10))
 		if args.check_in:
-			check_in = bot.find_element_by_xpath('/html/body/div[2]/main/div[2]/div/div/div/div/div/div[2]/div/div/div[3]/div/div[1]/div/div/div').click()
+			check_in = bot.find_element_by_xpath('/html/body/div[2]/main/div[2]/div/div/div/div/div/div[2]\
+													/div/div/div[3]/div/div[1]/div/div/div').click()
 		return bot
 
 	"""Needed incase of task reporting"""
@@ -54,6 +54,7 @@ class kw_login:
 		time.sleep(random.randrange(5,8))
 		kwv5_menu = bot.find_element_by_link_text('Kwantify V5').click()
 
+	""" Switch to specific tab depending on tab_key """
 	def get_tab(self, tab_key=None):
 		bot = self.bot
 		time.sleep(random.randrange(3,6))
@@ -64,6 +65,7 @@ class kw_login:
 			if tab_key in bot.current_url:
 				return opened_windows[i]
 
+	""" Get all assigned task links """
 	def get_task_links(self):
 		bot = self.bot
 		csm_tab = self.get_tab(tab_key='DashboardStandardv1')
@@ -80,6 +82,7 @@ class kw_login:
 		 				if 'TaskReporting' in link.get_attribute('href')]
 		return task_links
 
+	""" Iterate through task links and report task """
 	def report_project_task(self, links):
 		bot = self.bot
 		for link in links:
@@ -99,31 +102,35 @@ class kw_login:
 			""" Set To Remark """
 			remark = bot.find_element_by_id('txtARemarks')
 			remark.send_keys('completed')
-			""" Click on Submit button """
+			""" Click on Submit button & close current tab """
 			bot.find_element_by_id('btnSubmit').click()
 			alert_obj = bot.switch_to.alert
 			alert_obj.accept()
 			bot.close()
+			""" Switch back to task table page """
 			task_tab = self.get_tab(tab_key='framepage.aspx')
 			bot.switch_to.window(task_tab)
 
 	def kw_v6_logout(self):
 		bot = self.bot
-		time.sleep(random.randrange(6,10))
-		check_in = bot.find_element_by_xpath('//div[@class="csm-checkout"]').click()
+		check_out = bot.find_element_by_xpath('/html/body/div[2]/main/div[2]/div/div/div/div/div/div[2]\
+												/div/div/div[3]/div/div[2]/div/div/div').click()
 		time.sleep(random.randrange(3,6))
-		dropdown = bot.find_element_by_xpath('//*[@id="top_menu"]/li[5]/a').click()
+		user_dropdown = bot.find_element_by_xpath('/html/body/header/nav/ul[3]/li[3]/a').click()
 		time.sleep(random.randrange(2,5))
-		signout_menu = bot.find_element_by_xpath('//*[@id="o_logout"]').click()
+		signout_menu = bot.find_element_by_xpath('/html/body/header/nav/ul[3]/li[3]/div/a[3]').click()
+		time.sleep(random.randrange(2,5))
 		return bot
 
 if __name__ == "__main__":
 	k = kw_login(args.username, args.password)
 	resp = k.kw_v6_login()
-	k.kw_v5_login()
+	if args.report_task:
+		k.kw_v5_login()
+		task_links = k.get_task_links()
+		if len(task_links) > 0:
+			k.report_project_task(task_links)
 	if args.log_out:
 		time.sleep(random.randrange(5,10))
 		resp = k.kw_v6_logout()
-	task_links = k.get_task_links()
-	if len(task_links) > 0:
-		k.report_project_task(task_links)
+	resp.quit()
